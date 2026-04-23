89f8279675c751942ee0087a2f6fb943badda1b3
Juanfelipe0328 <jbustamante@estudiante.uniajc.edu.co>
2026-04-22 23:37:28 -0500

    Agregando la clase main

--- tallersegundocorte/src/main/java/com/uniajc/Main.java
+++ tallersegundocorte/src/main/java/com/uniajc/Main.java
@@ -1,7 +1,28 @@
 package com.uniajc;
 
+import com.uniajc.controlador.EstudianteController;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EstudianteController ec = new EstudianteController();
+        Scanner sc = new Scanner(System.in);
+        int op;
+
+        do {
+            System.out.println("""
+            1. Crear estudiante
+            2. Listar estudiantes
+            0. Salir
+            """);
+
+            op = sc.nextInt(); sc.nextLine();
+
+            if (op == 1) ec.crear();
+            if (op == 2) ec.listar();
+
+        } while (op != 0);
     }
 }
